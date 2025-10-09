07d45a747a09c78d9af0edcc968d517ddcb35305
Priyanshu Kumar <priyanshusurya164@gmail.com>
2025-10-09 15:52:13 +0530

    Update movies.py

--- movies.py
+++ movies.py
@@ -11,20 +11,10 @@ from youtubesearchpython import VideosSearch  # ✅ For trailer links
 movies = pd.read_csv("new_df.csv")
 
 # Create feature vectors
-cv = CountVectorizer(max_features=5000, stop_words='english')import requests
-import streamlit as st
-import pickle
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from youtubesearchpython import VideosSearch  # ✅ new import
-
-# Load movies dataset
-# Load movies dataset
-movies = pd.read_csv("new_df.csv")
-
-# Create feature vectors
 cv = CountVectorizer(max_features=5000, stop_words='english')
+
+# Load movies dataset
+
 vectors = cv.fit_transform(movies['tags']).toarray()
 
 
@@ -169,6 +159,7 @@ if st.button('Show Recommendation'):
         with cols[i]:
             st.text(f"{names[i]} \n⭐ {ratings[i]}")
             st.image(posters[i])
+
 
 
 
